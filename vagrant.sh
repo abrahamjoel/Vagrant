2e5144158557200fc7006edbabd7b78a10a4ff34
Joel Abraham <abrahamjoel219@gmail.com>
2019-02-27 00:45:00 +0530

    new

--- vagrant.sh
+++ vagrant.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install virtualbox
 sudo apt-get install vagrant
-vagrant box add ubuntu/trusty64
+vagrant box add ubuntu/xenial64
 #vagrant init ubuntu/trusty64 Or Vagrantfile from git could be copied to local
 sudo apt install git
 git clone https://github.com/abrahamjoel/Vagrant.git
