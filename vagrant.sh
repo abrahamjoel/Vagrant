3af999dfa8ce30fcb9f84cc9cd1666b74775308a
Joel Abraham <32274339+abrahamjoel@users.noreply.github.com>
2019-02-27 01:02:56 +0530

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -5,6 +5,7 @@ sudo apt-get install vagrant
 vagrant box add ubuntu/xenial64
 #vagrant init ubuntu/trusty64 Or Vagrantfile from git could be copied to local
 sudo apt install git
+cd ~
 git clone https://github.com/abrahamjoel/Vagrant.git
 cp Vagrant/Vagrantfile .
 cp Vagrant/playbook.sh .
